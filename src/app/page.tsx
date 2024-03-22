08aa7622cac6e32b0a0b894ed03812343ad2ce69
itzteeler <tylernguyen518@gmail.com>
2024-03-22 11:29:02 -0700

    LoginPage/Fonts/Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,21 @@ import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <div>
-      <h1>This is the login Page</h1>
+    <div className="loginBgImage">
+      <div className="grid grid-flow-row justify-center pt-36">
+        <div className="bg-white max-w-[31.625rem] rounded-lg">
+          <div className="pt-[20px] px-[43px] pb-[50px] text-center">
+            <p className="text-[48px] font-[DMSerifText]">MockTalks</p>
+            <p className="text-[20px] font-[Source-Sans-Pro]">Empower Your Success, One Mock Interview at a Time with MockTalks!</p>
+          </div>
+          <div>
+            <p>Username</p>
+            <input/>
+            <p>Password</p>
+            <input/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
