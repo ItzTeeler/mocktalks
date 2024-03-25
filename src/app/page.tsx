207c694984e6f4aecb8ae3ca4bb5dbde555ea1b6
itzteeler <tylernguyen518@gmail.com>
2024-03-25 14:30:25 -0700

    DataServices/Interfaces/Pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,50 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import backArrow from '../Assets/BackArrow.png'
+import { createAccount, getUserData, login } from "@/utils/Dataservices";
+import { IToken } from "@/Interfaces/Interfaces";
+import { useRouter } from "next/navigation";
+
 
 export default function LoginPage() {
   const [registerBool, setRegisterBool] = useState<boolean>(true);
   const [color, setColor] = useState<string>('text-[#1973E7]');
-  const [forgotBool, setForgotBool] = useState<boolean>(true);
+  const [forgotBool, setForgotBool] = useState<boolean>(false);
+
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  let router = useRouter();
+
   const handleRegister = () =>{
     setRegisterBool(!registerBool);
   }
   const handleForgotPassword = () => {
     setForgotBool(!forgotBool)
+  }
+  const handleSubmit = async () =>{
+    let userData = {
+      username: username,
+      password: password
+    }
+
+    if(registerBool){
+      console.log("Sign In")
+      // Logic for Sign In
+      let token: IToken = await login(userData);
+      if(token.token != null){
+        localStorage.setItem("Token", token.token);
+        getUserData(username);
+        router.push('/TestPage')
+      }else{
+        alert("Login Failed")
+      }
+
+    }else{
+      console.log("Register")
+      // Logic for Create Account
+      createAccount(userData);
+    }
   }
   
   useEffect(()=>{
@@ -21,11 +55,7 @@ export default function LoginPage() {
       setColor('text-[#FF0000]');
     }
   },[registerBool])
-  const handleBools = () => {
-    if(registerBool){
-
-    }
-  }
+  
   return (
     <div className="loginBgImage">
       <div className="grid grid-flow-row justify-center pt-20 pl-[8px] pr-[8px]">
@@ -33,21 +63,22 @@ export default function LoginPage() {
             
           <div className="pt-[20px] mb-[11px] md:mb-[50px] text-center">
           {registerBool ? "" : <div className="flex justify-end"><Image src={backArrow} alt="hi" onClick={handleRegister}/></div>}
+          {forgotBool ? <div className="flex justify-end"><Image src={backArrow} alt="hi" onClick={handleForgotPassword}/></div> : ""}
             <p className="text-[48px] font-[DMSerifText]">MockTalks</p>
             <p className="text-[20px] font-[Source-Sans-Pro]">Empower Your Success, One Mock Interview at a Time with MockTalks!</p>
           </div>
           <div className="">
             <p className="text-[20px] font-[DMSerifText] mb-[2px] md:mb-[10px]">Username</p>
-            <input className="h-[29px] md:h-[39px] mb-[17px] md:mb-[20px] w-full border-[1px] border-black rounded-[10px] text-[18px] font-[Source-Sans-Pro] pl-[16px]" placeholder="Enter UserID" required/>
+            <input type="text" onChange={(e) => setUsername(e.target.value)} className="h-[29px] md:h-[39px] mb-[17px] md:mb-[20px] w-full border-[1px] border-black rounded-[10px] text-[18px] font-[Source-Sans-Pro] pl-[16px]" placeholder="Enter UserID" required/>
             <p className="text-[20px] font-[DMSerifText] mb-[2px] md:mb-[10px]">Password</p>
-            <input className="h-[29px] md:h-[39px] mb-[17px] md:mb-[20px] w-full border-[1px] border-black rounded-[10px] text-[18px] font-[Source-Sans-Pro] pl-[16px]" placeholder="Enter Password" required/>
+            <input type="password" onChange={(e) => setPassword(e.target.value)} className="h-[29px] md:h-[39px] mb-[17px] md:mb-[20px] w-full border-[1px] border-black rounded-[10px] text-[18px] font-[Source-Sans-Pro] pl-[16px]" placeholder="Enter Password" required/>
           </div>
           <div className="">
           {registerBool ?  <p onClick={handleForgotPassword} className={`cursor-pointer ${color} text-[20px] mb-[19px] md:mb-[30px] font-[DMSerifText]`}>Forgot Password?</p> :  <p className={`${color} text-[20px] mb-[19px] md:mb-[30px] font-[DMSerifText]`}>Please choose a stronger password. Try a mix of letters, numbers, and symbols.</p>}
           </div>
-          <button className="text-[20px] font-[DMSerifText] bg-[#2B170C] text-white max-w-[419px] w-full text-center rounded-[10px]">{forgotBool ? "Change Password" : (registerBool ? "Login" : "Create Account")}</button>
+          <button onClick={handleSubmit} className="text-[20px] font-[DMSerifText] bg-[#2B170C] text-white max-w-[419px] w-full text-center rounded-[10px]">{forgotBool ? "Change Password" : (registerBool ? "Login" : "Create Account")}</button>
           <div className="mt-[24px] md:mt-[30px] mb-[75px] cursor-pointer">
-            <p onClick={handleRegister} className="font-[DMSerifText] text-[20px]">{registerBool ? "Not a member?" : "Already a member!"} <span className="font-[DMSerifText] text-[20px] text-[#1973E7]">{registerBool ? "Register?" : ""}</span></p>
+          <p onClick={handleRegister} className="font-[DMSerifText] text-[20px]">{forgotBool ? "" : (registerBool ? "Not a member?" : "Already a member!")} <span className="font-[DMSerifText] text-[20px] text-[#1973E7]">{forgotBool ? "" : (registerBool ? "Register?" : "Login")}</span></p>
           </div>
         </div>
       </div>
