803387a7f8deaf698005347663d9b4f46f694e26
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-03-26 09:02:30 -0700

    Work on Profile Page

--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -2,13 +2,27 @@
 
 import React from 'react'
 import {} from 'flowbite-react'
+import NavbarComponent from '../Components/NavbarComponent'
+import Image from 'next/image'
 
+import profileImgPlaceholder from '@/Assets/Ellipse.png'
 
 const page = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <NavbarComponent/>
+
+      <div className='flex justify-center p-12'>
+        <div className='bg-[#FFFFFF] w-full h-80 rounded-2xl'>
+          <div className='grid grid-cols-3 gap-4'>
+            <div className='flex justify-center align-middle'>
+            <Image src={profileImgPlaceholder} className='w-[300px]' alt='profileImg'/>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </>
   )
 }
 
