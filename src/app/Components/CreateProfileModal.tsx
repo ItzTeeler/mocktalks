a74036209e2818b211cb4d5255f5fd6fa5ee51c3
itzteeler <tylernguyen518@gmail.com>
2024-03-26 15:06:07 -0700

    CreateProfileModal/DropzoneComponent/DropDownComponent

---
+++ src/app/Components/CreateProfileModal.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { Button, FileInput, Modal } from "flowbite-react";
+import { useState } from "react";
+import { DropDownComponent } from "./DropDownComponent";
+import { DropZoneComponent } from "./DropZoneComponent";
+
+
+export function CreateProfileModal() {
+  const [openModal, setOpenModal] = useState(false);
+  const [profileImg, setProfileImg] = useState<string>("");
+
+  const handleImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let reader = new FileReader();
+    const file = e.target.files?.[0];
+
+    if (file) {
+      reader.onload = () => {
+        setProfileImg(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setProfileImg("");
+    }
+  };
+
+
+  return (
+    <div className="font-[DMSerifText]">
+      <Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
+      <Modal show={openModal} onClose={() => setOpenModal(false)}>
+        <p className="font-[DMSerifText] text-[30px] px-[24px] pt-[15px]">Create Profile</p>
+        <Modal.Body className="font-[DMSerifText] text-[30px] pt-0">
+          <div>
+            <div className="grid grid-cols-3">
+                <div className="flex flex-col justify-end">
+                    <p className="mb-[10px]">First Name</p>
+                    <input className="mr-[18px] mb-[10px] rounded-[10px]" type="text" />
+                </div>
+                <div className="flex flex-col justify-end">
+                    <p className="mb-[10px]">Last Name</p>
+                    <input type="text" className="mr-[54px] mb-[10px] rounded-[10px]"/>
+                </div>
+                <div>
+                <DropZoneComponent/>
+                </div>
+            </div>
+          </div>
+          <div className="flex flex-col">
+          <div className="flex flex-col">
+                    <p className="mb-[10px]">What city do you live in?</p>
+                    <input type="text" className="rounded-[10px] mb-[10px]"/>
+                </div>
+          <div className="flex flex-col">
+                    <p className="mb-[10px]">Education</p>
+                    <input type="text" className="rounded-[10px] mb-[10px]"/>
+                </div>
+          <div className="flex flex-col">
+                    <p className="mb-[10px]">Years of Experience</p>
+                    <input type="text" className="rounded-[10px] mb-[10px]"/>
+                </div>
+          <div className="flex flex-col">
+                    <p className="mb-[10px]">What is your current level at job interviews?</p>
+                    <DropDownComponent/>
+                </div>
+          </div>
+        </Modal.Body>
+        <div className="flex justify-end pb-[15px] pr-[24px] pt-[10px]">
+            <button onClick={() => setOpenModal(false)} className="text-[30px] bg-[#2B170C] font-[DMSerifText] text-white rounded-[10px] px-[10px] py-[6px] ">Create</button>
+        </div>
+      </Modal>
+    </div>
+  );
+}
--- src/app/TestPage/page.tsx
+++ src/app/TestPage/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { CreateProfileModal } from '../Components/CreateProfileModal'
 
 const TestPage = () => {
   return (
     <div>
       <h1>This is a test page</h1>
+      <CreateProfileModal/>
     </div>
   )
 }
