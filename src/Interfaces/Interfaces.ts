4060f8a292d3a58df1a35c50a2270ca5ecfa83f2
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-03-26 15:08:50 -0700

    Add Components

--- src/Interfaces/Interfaces.ts
+++ src/Interfaces/Interfaces.ts
@@ -11,3 +11,10 @@ export interface IUserData{
     userId: number
     publicName: string
 }
+
+export interface IMockInterviewProps{
+    date: string, 
+    type: string, 
+    questions: string, 
+    language: string
+}
