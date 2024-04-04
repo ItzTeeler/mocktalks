3d2dfea54969888fa8389b59e72f36d171723b4a
itzteeler <tylernguyen518@gmail.com>
2024-04-04 14:11:32 -0700

    commit

--- src/app/Components/NavbarComponent.tsx
+++ src/app/Components/NavbarComponent.tsx
@@ -7,25 +7,26 @@ import { Navbar } from "flowbite-react";
 import Image from "next/image";
 
 import icon from '@/Assets/MockTalkIcon.png'
-import bellIcon from '@/Assets/BellIcon.svg'
-import chatDots from '@/Assets/ChatCenteredDots.svg'
+import bellIcon from '@/Assets/BellIcon.png'
+import chatDots from '@/Assets/ChatCenteredDots.png'
 
 export default function NavbarComponent() {
   return (
     <Navbar fluid>
       <Navbar.Brand as={Link} href="/Profile">
-        <Image src={icon} className="mr-1 sm:mr-3 h-3 w-auto sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap sm:text-4xl dark:text-white font-[DMSerifText]">MockTalks</span>
+        <Image src={icon} className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+        <span className="self-center whitespace-nowrap text-[36px] dark:text-white font-[DMSerifText]">MockTalks</span>
       </Navbar.Brand>
+      <Navbar.Toggle />
       <Navbar.Collapse>
         <Navbar.Link href="#">
-          <Image src={bellIcon} alt="Bell Icon" />
+            <Image src={bellIcon} alt="Bell Icon"/>
         </Navbar.Link>
         <Navbar.Link href="/MessagingPage">
             <Image src={chatDots} alt="Centered Chat Dots"/>
         </Navbar.Link>
         <Navbar.Link href="/">
-          <span className="self-center whitespace-nowrap text-[36px] dark:text-white font-[DMSerifText]">Logout</span>
+        <span className="self-center whitespace-nowrap text-[36px] dark:text-white font-[DMSerifText]">Logout</span>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
