28344556b20f53a4ac8e8df34442517bd1ee3bd5
itzteeler <tylernguyen518@gmail.com>
2024-04-04 14:11:32 -0700

    commit

--- src/app/MessagingPage/page.tsx
+++ src/app/MessagingPage/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavbarComponent from '../Components/NavbarComponent'
 
 const MessagingPage = () => {
   return (
     <div>
-      
+      <NavbarComponent/>
     </div>
   )
 }
