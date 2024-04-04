90f7041164f6b0b2ed7ede42cbcf0b8a645afc55
itzteeler <tylernguyen518@gmail.com>
2024-04-04 15:46:27 -0700

    Messaging Card/Inputs/Page

---
+++ src/app/Components/MessagingTextInputComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MessagingTextInputComponent = () => {
+  return (
+    <input type="text" className=''/>
+  )
+}
+
+export default MessagingTextInputComponent
--- src/app/MessagingPage/page.tsx
+++ src/app/MessagingPage/page.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import NavbarComponent from '../Components/NavbarComponent'
+import MessagingSearchInputComponent from '../Components/MessagingSearchInputComponent'
+import MessagingPeopleCardComponent from '../Components/MessagingPeopleCardComponent'
+import MessagingTextInputComponent from '../Components/MessagingTextInputComponent'
 
 const MessagingPage = () => {
   return (
-    <div>
+    <div className='bg-[#696969] h-full'>
       <NavbarComponent/>
+      <div className='flex p-[10px] h-screen'>
+        <div className='bg-[#ffffff] border-r-4 w-[541px] border-black rounded-tl-[15px] rounded-bl-[15px]'>
+            <MessagingSearchInputComponent/>
+            <MessagingPeopleCardComponent/>
+        </div>
+        <div className='bg-[#ffffff] w-full rounded-tr-[15px] rounded-br-[15px]'>
+        <MessagingTextInputComponent/>
+        </div>
+      </div>
     </div>
   )
 }
