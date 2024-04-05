78dd64167e9fb25792981e6392a7263203354076
itzteeler <tylernguyen518@gmail.com>
2024-04-05 09:18:01 -0700

    editmodal/messaginginput/assets

--- src/app/MessagingPage/page.tsx
+++ src/app/MessagingPage/page.tsx
@@ -5,20 +5,20 @@ import MessagingPeopleCardComponent from '../Components/MessagingPeopleCardCompo
 import MessagingTextInputComponent from '../Components/MessagingTextInputComponent'
 
 const MessagingPage = () => {
-  return (
-    <div className='bg-[#696969] h-full'>
-      <NavbarComponent/>
-      <div className='flex p-[10px] h-screen'>
-        <div className='bg-[#ffffff] border-r-4 w-[541px] border-black rounded-tl-[15px] rounded-bl-[15px]'>
-            <MessagingSearchInputComponent/>
-            <MessagingPeopleCardComponent/>
+    return (
+        <div className='bg-[#696969] h-full'>
+            <NavbarComponent />
+            <div className='grid grid-cols-6 p-[10px]'>
+                <div className='col-span-2 bg-[#ffffff] w-full border-r-4 h-[88vh] border-black rounded-tl-[15px] rounded-bl-[15px]'>
+                    <MessagingSearchInputComponent />
+                    <MessagingPeopleCardComponent />
+                </div>
+                <div className='col-span-4 bg-[#ffffff] w-full h-[88vh] rounded-tr-[15px] rounded-br-[15px] flex items-end'>
+                    <MessagingTextInputComponent />
+                </div>
+            </div>
         </div>
-        <div className='bg-[#ffffff] w-full rounded-tr-[15px] rounded-br-[15px]'>
-        <MessagingTextInputComponent/>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default MessagingPage
