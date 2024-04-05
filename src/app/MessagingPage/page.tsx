98aa164c211917d41d46e0c71bb697c70e7656ee
itzteeler <tylernguyen518@gmail.com>
2024-04-05 09:52:52 -0700

    Commit

--- src/app/MessagingPage/page.tsx
+++ src/app/MessagingPage/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+
 import React from 'react'
 import NavbarComponent from '../Components/NavbarComponent'
-import MessagingSearchInputComponent from '../Components/MessagingSearchInputComponent'
+
+
+
 import MessagingPeopleCardComponent from '../Components/MessagingPeopleCardComponent'
+import MessagingSearchInputComponent from '../Components/MessagingSearchInputComponent'
 import MessagingTextInputComponent from '../Components/MessagingTextInputComponent'
 
 const MessagingPage = () => {
@@ -10,11 +15,11 @@ const MessagingPage = () => {
             <NavbarComponent />
             <div className='grid grid-cols-6 p-[10px]'>
                 <div className='col-span-2 bg-[#ffffff] w-full border-r-4 h-[88vh] border-black rounded-tl-[15px] rounded-bl-[15px]'>
-                    <MessagingSearchInputComponent />
-                    <MessagingPeopleCardComponent />
+                    <MessagingSearchInputComponent/>
+                    <MessagingPeopleCardComponent/>
                 </div>
                 <div className='col-span-4 bg-[#ffffff] w-full h-[88vh] rounded-tr-[15px] rounded-br-[15px] flex items-end'>
-                    <MessagingTextInputComponent />
+                    <MessagingTextInputComponent/>
                 </div>
             </div>
         </div>
