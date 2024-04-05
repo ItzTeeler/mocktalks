5fc5686f1e073c4e8c77f3aa6f8bceee1de10a57
itzteeler <tylernguyen518@gmail.com>
2024-04-05 15:49:10 -0700

    Bubble Comp, Video Cam Comp, Footer Comp, Assets

--- src/app/Components/MessagingTextInputComponent.tsx
+++ src/app/Components/MessagingTextInputComponent.tsx
@@ -5,12 +5,12 @@ import SendIcon from '@/Assets/MessagingSend.png'
 
 const MessagingTextInputComponent = () => {
     return (
-        <div className='w-full relative'>
+        <div className='w-full relative fixed bottom'>
             <div className='flex items-center inset-y-0 mx-[43px] pt-[30px] pb-[40px]'>
                 <div className='absolute p-2'>
                     <Image src={FileAdd} alt="Search Icon" />
                 </div>
-                <input type="text" className='rounded-[10px] w-full px-12 py-[12px] text-left text-[18px] text-black' placeholder='Enter Message' />
+                <input type="text" className='font-[Source-Sans-Pro] rounded-[10px] w-full px-12 py-[12px] text-left text-[18px] text-black' placeholder='Enter Message' />
                 <div className='absolute right-14'>
                     <Image src={SendIcon} alt="Send Message" className='' />
                 </div>
--- src/app/MessagingPage/page.tsx
+++ src/app/MessagingPage/page.tsx
@@ -7,22 +7,43 @@ import MessagingPeopleCardComponent from '../Components/MessagingPeopleCardCompo
 import MessagingTextInputComponent from '../Components/MessagingTextInputComponent'
 import VideoIcon from '@/Assets/MessagingWebCam.png'
 import Image from 'next/image'
+import MessagingBubblesComponent from '../Components/MessagingBubblesComponent'
+import MessagingBubbleComponentSender from '../Components/MessagingBubbleComponentSender'
 
 const MessagingPage = () => {
     return (
         <div className='bg-[#696969] h-full'>
             <NavbarComponent />
             <div className='grid grid-cols-6 p-[10px]'>
-                <div className='col-span-2 bg-[#ffffff] w-full border-r-4 h-[88vh] border-black rounded-tl-[15px] rounded-bl-[15px]'>
+                <div className='col-span-2 bg-[#ffffff] w-full border-r-[3px] h-screen border-black rounded-tl-[15px] rounded-bl-[15px] overflow-y-scroll'>
                     <MessagingSearchInputComponent/>
                     <MessagingPeopleCardComponent/>
+                    <MessagingPeopleCardComponent/>
+                    <MessagingPeopleCardComponent/>
+                    <MessagingPeopleCardComponent/>
+                    <MessagingPeopleCardComponent/>
+                    <MessagingPeopleCardComponent/>
+                    <MessagingPeopleCardComponent/>
                 </div>
-                <div className='col-span-4 bg-[#ffffff] w-full h-[88vh] rounded-tr-[15px] rounded-br-[15px] flex flex-col'>
-                    <div className='bg-[#D9D9D9] w-full flex justify-between'>
-                        <p>Name</p>
+                <div className='col-span-4 bg-[#ffffff] w-full h-screen rounded-tr-[15px] rounded-br-[15px] flex flex-col justify-between'>
+                    <div className='bg-[#D9D9D9] text-[58px] font-[DMSerifText] w-full rounded-tr-[15px] px-[50px] py-[32px] flex justify-between items-center z-10'>
+                        <p>Tyler Nguyen</p>
                         <Image src={VideoIcon} alt="Video Icon"/>
                     </div>
-                    <div>
+    
+        
+                    <div className='overflow-y-scroll'>
+                        <MessagingBubblesComponent/>
+                        <MessagingBubblesComponent/>
+                        <MessagingBubblesComponent/>
+                        <MessagingBubblesComponent/>
+                        <MessagingBubblesComponent/>
+                        <MessagingBubblesComponent/>
+                        <MessagingBubblesComponent/>
+                        <MessagingBubblesComponent/>
+                        <div className='flex-'>
+                        <MessagingBubbleComponentSender/>
+                        </div>
                         <MessagingTextInputComponent/>
                     </div>
                 </div>
