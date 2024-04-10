ccf585acbb3c952edcfad8d7c6e5f4e5a627be65
itzteeler <tylernguyen518@gmail.com>
2024-04-10 15:48:24 -0700

    bubbles/responsive/modals

--- src/app/TestPage/page.tsx
+++ src/app/TestPage/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { CreateProfileModal } from '../Components/CreateProfileModal'
 import { EditProfileModal } from '../Components/EditProfileModal'
+import { ScheduleInterviewComponent } from '../Components/ScheduleInterviewComponent'
+
 
 const TestPage = () => {
   return (
     <div>
       <h1>This is a test page</h1>
       <CreateProfileModal/>
+      <EditProfileModal/>
+      <ScheduleInterviewComponent/>
     </div>
   )
 }
