7e017e5ad0c83f243d62e7ae76485b0ecc529986
itzteeler <tylernguyen518@gmail.com>
2024-04-30 13:58:58 -0700

    Reschedule Calendar/ Schedule Comp

--- src/app/Components/ScheduleComponent.tsx
+++ src/app/Components/ScheduleComponent.tsx
@@ -3,15 +3,16 @@
 import { IAppointments, IMockInterviewProps, ScheduleComponentProps } from '@/Interfaces/Interfaces';
 import { Button } from 'flowbite-react';
 import React from 'react'
+import ReScheduleComponent from './ReScheduleComponent';
 
-const ScheduleComponent = (props: ScheduleComponentProps) => {
+const ScheduleComponent = (props: ScheduleComponentProps & { submitBool: () => void }) => {
     return (
         <>
             <div className='hidden min-[1440px]:block'>
                 <hr />
                 <div className='grid grid-flow-col p-3'>
                     <div className='w-80 min-[1440px]:w-40 2xl:w-80'>
-                        <p className='text-4xl text-black font-[Source-Sans-Pro] min-[1440px]:text-2xl 2xl:text-4xl'>{props.selectedDate}</p>
+                        <p className='text-4xl text-black font-[Source-Sans-Pro] min-[1440px]:text-2xl 2xl:text-4xl'>{props.selectedDate} {props.time}</p>
                     </div>
                     <div className='w-48 min-[1440px]:w-40 2xl:w-48'>
                         <p className='text-4xl text-black font-[Source-Sans-Pro] min-[1440px]:text-2xl 2xl:text-4xl'>{props.typePractice}</p>
@@ -25,7 +26,7 @@ const ScheduleComponent = (props: ScheduleComponentProps) => {
                     <div className='w-[480px] min-[1440px]:w-[320px] 2xl:w-[480px]'>
                         <div className='flex flex-row space-x-4'>
                             <Button className='bg-[#D9D9D9] w-48'><span className='text-black text-4xl font-[Source-Sans-Pro]'>Cancel</span></Button>
-                            <Button className='bg-[#2B170C] w-full'><span className='text-white text-4xl font-[Source-Sans-Pro]'>Reschedule</span></Button>
+                            <ReScheduleComponent id={props.id} submitBool={props.submitBool} />              
                         </div>
                     </div>
                 </div>
@@ -53,7 +54,7 @@ const ScheduleComponent = (props: ScheduleComponentProps) => {
                     </div>
                     <div className='grid grid-row-5 bg-white'>
                         <div className='flex items-center pl-5 max-[300px]:pl-1 font-[Source-Sans-Pro] text-xl h-16'>
-                            {props.selectedDate}
+                            {props.selectedDate} {props.time}
                         </div>
                         <div className='flex items-center pl-5 max-[300px]:pl-1 font-[Source-Sans-Pro] text-xl h-16'>
                             {props.typePractice}
@@ -67,7 +68,7 @@ const ScheduleComponent = (props: ScheduleComponentProps) => {
                         <div className='flex items-center max-[300px]:pl-1 px-2 font-[Source-Sans-Pro] text-xl h-16'>
                             <div className='w-full flex flex-col space-y-1'>
                                 <Button className='bg-[#D9D9D9] text-black font-[Source-Sans-Pro] rounded-full h-6'>Cancel</Button>
-                                <Button className='bg-[#2B170C] text-white font-[Source-Sans-Pro] rounded-full h-6'>Reschedule</Button>
+                                <ReScheduleComponent id={props.id} submitBool={props.submitBool} />                               
                             </div>
                         </div>
                     </div>
