923ace1540456610cd50c29eb24711410aab142a
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-05-14 15:31:33 -0700

    Fix Notification Component

--- src/app/Components/PendingNotificationComponent.tsx
+++ src/app/Components/PendingNotificationComponent.tsx
@@ -2,23 +2,27 @@
 
 import React, { useState } from "react"
 import { Button } from "flowbite-react"
+import { useRouter } from "next/navigation";
 
-export function PendingNotificationComponent() {
-    const [visible, setVisible] = useState<boolean>(true);
+export function PendingNotificationComponent(props: {open: boolean, close: React.Dispatch<React.SetStateAction<boolean>>}) {
     const [name, setName] = useState<string>('John Fartgroom');
     const [experience, setExperience] = useState<string>('Beginner');
-    
-    const handleAcceptButton = () => {
-        setVisible(false);
+
+    const router = useRouter();
+
+    const handleClose = () => {
+        props.close(false);
     }
-    const handleCancelButton = () => {
-        setVisible(false);
+    
+    const handleSubmit = () => {
+        props.close(false);
+        router.push(`/MessagingPage`);
     }
 
     return (
         <>
         {
-            visible == true &&
+            props.open == true &&
             <div className={`sm:absolute sm:right-12`}>
                 <div className="w-full sm:w-[360px] h-[84px] bg-white border-black border-2 px-5 py-2 shadow-xl">
                     <div className="grid grid-cols-2">
@@ -28,9 +32,9 @@ export function PendingNotificationComponent() {
                         </div>
                         <div className="w-full flex justify-end">
                             <div className="grid grid-rows-2">
-                                <Button className="bg-[#2B170C] text-white rounded-full w-[64px] h-[24px] text-[12px] font-[Source-Sans-Pro]" onClick={handleAcceptButton}>Accept</Button>
+                                <Button className="bg-[#2B170C] text-white rounded-full w-[64px] h-[24px] text-[12px] font-[Source-Sans-Pro]" onClick={handleSubmit}>Accept</Button>
                                 <div className="flex items-end">
-                                    <Button className="bg-[#D9D9D9] text-black rounded-full w-[64px] h-[24px] text-[12px] font-[Source-Sans-Pro]" onClick={handleCancelButton}>Cancel</Button>
+                                    <Button className="bg-[#D9D9D9] text-black rounded-full w-[64px] h-[24px] text-[12px] font-[Source-Sans-Pro]" onClick={handleClose}>Cancel</Button>
                                 </div>
                             </div>
                         </div>
