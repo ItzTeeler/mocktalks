245be78943a63eb29a251195601974cbd678d4e2
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-05-15 11:22:43 -0700

    moved messaging to messaging page

--- src/app/MessagingPage/page.tsx
+++ src/app/MessagingPage/page.tsx
@@ -11,6 +11,8 @@ import MessagingBubblesComponent from '../Components/MessagingBubblesComponent'
 import MessagingBubbleComponentSender from '../Components/MessagingBubbleComponentSender'
 import MessageLeave from '@/Assets/MessagesBackArrow.png'
 import { useRouter } from 'next/navigation'
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr'
+import { IMessages } from '@/Interfaces/Interfaces'
 
 const MessagingPage = () => {
     const [hiddenOrBlock, setHiddenOrBlock] = useState<string>("hidden");
@@ -43,6 +45,71 @@ const MessagingPage = () => {
     const handleVideoClick = () => {
         router.push('/VideoChat')
     }
+
+
+
+
+
+
+    const [conn, setConnection] = useState<HubConnection>();
+    const [messages, setMessages] = useState<IMessages[]>([]);
+    const [message, setMessage] = useState<string>("")
+    const [usersname, setUsersname] = useState<string>();
+    const [chatroom, setChatroom] = useState<string>();
+
+    const joinChatRoom = async (usersname: string, chatroom: string) => {
+        try {
+            const conn: HubConnection = new HubConnectionBuilder()
+                .withUrl("http://localhost:5150/chat")
+                .configureLogging(LogLevel.Information)
+                .build();
+            // const conn = new HubConnectionBuilder()
+            //   .withUrl("https://mocktalksapihosting.azurewebsites.net/chat")
+            //   .configureLogging(LogLevel.Information)
+            //   .build();
+
+            // conn.on("JoinSpecificChatRoomMsg", (usersname: string, msg: string) => {
+            //   sendMessage(msg);
+            // })
+
+            conn.on("RecieveSpecificMessage", (usersname: string, msg: string) => {
+                setMessages(messages => [...messages, { usersname, msg }]);
+            });
+
+            await conn.start();
+            await conn.invoke("JoinSpecificChatRoom", { usersname, chatroom })
+
+            setConnection(conn);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const sendMessage = async (message: string) => {
+        try {
+            conn && await conn.invoke("SendMessage", message);
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const submitFunc = () => {
+        if (usersname !== undefined && chatroom !== undefined) {
+            joinChatRoom(usersname, chatroom);
+        }
+    }
+
+    const submitMessage = () => {
+        sendMessage(message);
+        setMessage("");
+    }
+
+
+
+
+
+
+
     return (
         <div className='bg-[#696969] h-full'>
             <NavbarComponent />
@@ -87,6 +154,55 @@ const MessagingPage = () => {
                     </div>
                 </div>
             </div>
+
+
+
+
+
+
+
+            <div>
+                <p>Welcome to chat</p>
+
+                {
+                    !conn ?
+                        <div>
+                            <input className='border-black border-2' type='text' onChange={e => { setUsersname(e.target.value) }} />
+                            <input className='border-black border-2' type='text' onChange={e => { setChatroom(e.target.value) }} />
+                            <button className="text-red-500" onClick={() => submitFunc()}>submit</button>
+                        </div>
+                        :
+                        <div className='grid'>
+                            <p>Chatroom</p>
+
+                            <div>
+                                {
+                                    messages && messages.map(
+                                        (msg: { msg: string; usersname: string }, index: number) => {
+                                            return (
+                                                <div key={index}>
+                                                    <p>{msg.msg} - {msg.usersname}</p>
+                                                </div>
+                                            )
+                                        }
+                                    )
+                                }
+                            </div>
+
+                            <div>
+                                <input className='border-black border-2' type="text" onChange={e => setMessage(e.target.value)} value={message} placeholder='type a message' />
+                                <button className=" text-red-500  " onClick={() => submitMessage()} disabled={!message}>submit</button>
+                            </div>
+                        </div>
+                }
+            </div>
+
+
+
+
+
+
+
         </div>
     )
 }
