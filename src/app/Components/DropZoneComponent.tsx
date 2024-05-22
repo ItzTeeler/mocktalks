27567e2875cab67df571d93c395f8fcd317c36fb
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-05-22 15:38:44 -0700

    Push changes

--- src/app/Components/DropZoneComponent.tsx
+++ src/app/Components/DropZoneComponent.tsx
@@ -1,10 +1,10 @@
 
 "use client";
+
 import { IDropZoneImage } from "@/Interfaces/Interfaces";
 import { FileInput, Label } from "flowbite-react";
 import { useState } from "react";
 import AccountCreateComponent from "./AccountCreateComponent";
-import Image from "next/image";
 
 export function DropZoneComponent(props: IDropZoneImage) {
   const [alertBool, setAlertBool] = useState<string>("hidden");
@@ -77,7 +77,7 @@ export function DropZoneComponent(props: IDropZoneImage) {
                 d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
               />
             </svg>
-            <Image src={previewImage} alt="Preview Image" width={150} height={150} className="absolute rounded-full hover:opacity-50" />
+            <img src={previewImage} alt="Preview Image" className={`${imageVisible == true ? "block" : "hidden"} absolute w-[150px] h-[150px] object-cover rounded-full hover:opacity-50 z-0`}/>
           </div>
           <FileInput onChange={handleImage} accept='image/png, image/jpeg' id="dropzone-file" className="hidden" />
         </Label>
