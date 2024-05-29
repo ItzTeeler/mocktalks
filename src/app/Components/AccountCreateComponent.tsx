76f681ab46cad709c7e5ddaa8bb8b0d02a964c7f
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-05-28 21:17:58 -0700

    fixed alert modal

--- src/app/Components/AccountCreateComponent.tsx
+++ src/app/Components/AccountCreateComponent.tsx
@@ -4,7 +4,7 @@ const AccountCreateComponent = (props: {show: string; text: string;}) => {
 
     return (
         <div className={`${props.show}`}>
-            <div className='absolute flex justify-center min-w-full mt-5 left-0 z-10 top-0'>
+            <div className='fixed flex justify-center min-w-full mt-5 left-0 z-10 top-0'>
                 <div className='bg-white border border-black rounded-[10px] px-[50px] py-[20px]'>
                     <p className='text-center'>{props.text}</p>
                 </div>
