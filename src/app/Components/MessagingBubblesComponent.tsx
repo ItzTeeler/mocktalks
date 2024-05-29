5a080e6f1ab1ab124493155fd033277bb079c7d2
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-05-29 15:00:00 -0700

    messaging reciever update

--- src/app/Components/MessagingBubblesComponent.tsx
+++ src/app/Components/MessagingBubblesComponent.tsx
@@ -21,18 +21,20 @@ const MessagingBubblesComponent = (props: IPropMessageSplitter) => {
     <div className='bg-[#696969] m-5 rounded-[15px] max-w-[80%] lg:max-w-[50%] p-[15px]'>
       {
         userData ?
-        <div className='flex flex-row items-center gap-2'>
-          <div className=''>
-            <img src={userData.profileImg} alt='ProfilePic' className='rounded-full min-w-[80px] w-[80px] h-[80px] min-h-[80px]' />
+          <div className='flex flex-row items-center gap-2'>
+            <div className=''>
+              <img src={userData.profileImg} alt='ProfilePic' className='rounded-full min-w-[80px] w-[80px] h-[80px] min-h-[80px]' />
+            </div>
+            <div className=''>
+              <p className='text-[12px] font-[Source-Sans-Pro] overflowW text-[#ffffff]'>{userData.fullName}</p>
+              <p className='text-[18px] font-[Source-Sans-Pro] overflowW text-[#ffffff]'>{props.dataPass.text}</p>
+            </div>
           </div>
-          <div className=''>
-            <p className='text-[12px] font-[Source-Sans-Pro] overflowW text-[#ffffff]'>{userData.fullName}</p>
-            <p className='text-[18px] font-[Source-Sans-Pro] overflowW text-[#ffffff]'>{props.dataPass.text}</p>
+          :
+          <div>
+            <p className='text-[18px] text-white font-[Source-Sans-Pro] mb-5'>Loading Name...</p>
+            <p className='text-[18px] text-white font-[Source-Sans-Pro] mb-5'>Loading Text...</p>
           </div>
-        </div>
-        :
-        <p className='text-[18px] text-white font-[Source-Sans-Pro]'>Loading...</p>
-
       }
     </div>
   )
